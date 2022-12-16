f25a431bfc383f10472c250b506f8409ce8ec356
Katoribako <64684310+Katoribako@users.noreply.github.com>
2022-12-16 16:15:58 +0300

    Was completed first task

---
+++ Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая на вход принимает два числа
+//и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Please input a first number: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please input a second number: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA > numberB){Console.Write(numberA + " bigger than " + numberB);}
+else{Console.Write(numberB + " bigger than " + numberA);}
