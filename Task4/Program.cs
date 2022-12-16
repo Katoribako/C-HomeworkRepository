117ac738ea29ac1405c8ed2de785556e5b822488
Katoribako <64684310+Katoribako@users.noreply.github.com>
2022-12-16 18:51:04 +0300

    Added fourth task

---
+++ Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Please input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int current = 1;
+while(current <= number)
+{
+    if(current % 2 == 0)
+    {
+    Console.Write(current + " ");
+    current = current + 2;
+    }
+    else
+    {
+    current = current + 1;
+    }
+}
