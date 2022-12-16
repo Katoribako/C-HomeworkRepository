52eb2b92925a83b43e8812926a776d97dcf03710
Katoribako <64684310+Katoribako@users.noreply.github.com>
2022-12-16 16:50:24 +0300

    Completed third task

---
+++ Task3/Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт,
+//является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Please input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0){Console.Write(number + " is even number");}
+else{Console.Write(number + " is not even number");}
