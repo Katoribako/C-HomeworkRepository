f47b186bfb99dd149bb069cdd08b6a976e19107b
Katoribako <64684310+Katoribako@users.noreply.github.com>
2022-12-26 19:22:43 +0300

    added task8

---
+++ Task8/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+
+bool polidrome(int num)
+{
+    int reversivenum = 0;
+    int memorynum = num;
+    while (num > 0)
+    {
+        int lastnum = num % 10;
+        reversivenum = reversivenum * 10 + lastnum;
+        num = num / 10;
+    }
+    return reversivenum == memorynum;
+}
+
+Console.WriteLine("Please input your number ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (polidrome(num))Console.WriteLine($"{num} is polidrome");
+else Console.WriteLine($"{num} is not polidrome");
