cc00c8e1af31e3b7a2d078cde244f58864b15be4
Katoribako <64684310+Katoribako@users.noreply.github.com>
2023-01-13 17:17:49 +0300

    added Task 11 Correct Version

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,19 +1,23 @@
 ﻿/*Напишите цикл, который принимает на вход два числа (A и B)
 и возводит число A в натуральную степень B.*/
 
-double exponentiation (double num, double exp)
+int power(int num, int pwr)
 {
-    double result = Math.Pow(num, exp);
-    return result;
+    int summ = 1;
+    for(int current = 1; current <= pwr; current++)
+    {
+       summ *= num;
+    }
+    return summ;
 }
 Console.WriteLine("Write a loop that takes two numbers (A and B) as input");
 Console.WriteLine("and raises A to the power of B.");
 Console.WriteLine();
 Console.WriteLine("Please input first number ");
-double number = Convert.ToDouble(Console.ReadLine());
+int number = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Please input second number ");
-double power = Convert.ToDouble(Console.ReadLine());
-if (number != 0){double result = exponentiation(number, power);
+int pwr = Convert.ToInt32(Console.ReadLine());
+if (number != 0){int result = power(number, pwr);
 Console.WriteLine($"Result is {result}");}
 else{Console.WriteLine("Please input correct number");}
 
