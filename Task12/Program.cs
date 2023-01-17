70c184062ce07f68bd6b8abd37b5ac193ea5a206
Katoribako <64684310+Katoribako@users.noreply.github.com>
2023-01-17 14:29:04 +0300

    added Task 12

---
+++ Task12/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая принимает на вход число
+и выдаёт сумму цифр в числе.*/
+    int summ(int num)
+{
+    int sum = 0;
+    for(int i = num; i != 0; i /= 10)
+    {
+        sum += i % 10;
+    }
+    return sum;
+}
+Console.WriteLine("Please input a number ");
+int numb = Convert.ToInt32(Console.ReadLine());
+int sum = summ(numb);
+Console.WriteLine($"sum of all digits of a number {numb} is {sum}");
