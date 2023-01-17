9f4b4c1b0b4dd99669f96d2df96b7dd223984418
Katoribako <64684310+Katoribako@users.noreply.github.com>
2023-01-17 15:47:36 +0300

    added Task 13

---
+++ Task13/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
+
+int[] CreateArray(int length)
+{
+    int[] array = new int[length];
+
+    for(int i = 0; i < length; i++)
+    {
+        Console.Write($"Input a {i + 1} element: ");
+        array[i]= Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+void ShowArray(int[] array) 
+{ 
+    Console.Write("Your array is [ " ); 
+    for (int i = 0; i < array.Length - 1; i++) 
+        Console.Write(array[i] + " | "); 
+    Console.Write($"{array[array.Length - 1]} ]"); 
+    Console.WriteLine(); 
+}
+Console.WriteLine("Please input a length of array");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArray(m);
+ShowArray(array);
+
+
