69daac7d8d4d10b75418c346e905151a3092be99
Katoribako <64684310+Katoribako@users.noreply.github.com>
2023-01-20 19:29:55 +0300

    added Task 16

---
+++ Task16/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задайте массив вещественных чисел.
+Найдите разницу между максимальным и минимальным элементов массива.*/
+
+double[] CreateRandomArray(int length, int minValue, int maxValue) 
+ { 
+     double[] array = new double[length]; 
+
+     for(int i = 0; i < length; i++) 
+     { 
+         array[i] = new Random().Next(minValue, maxValue) + new Random().NextDouble(); 
+     } 
+     return array; 
+ }
+void ShowArray(double[] array) 
+{ 
+    Console.Write("Your array is [ " ); 
+    for (int i = 0; i < array.Length - 1; i++)
+
+        Console.Write(array[i] + " | "); 
+    Console.Write($"{array[array.Length - 1]} ]"); 
+    Console.WriteLine(); 
+}
+void MinMax(double[] array)
+{
+    double[] arr = array;
+    Console.WriteLine($"Minimum number is {arr.Min()}");
+    Console.WriteLine($"Maximum number is {arr.Max()}");
+}
+Console.WriteLine("Please input a length of array");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please input a min of array");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please input a max of array");
+int max = Convert.ToInt32(Console.ReadLine());
+double [] arr = CreateRandomArray(size, min, max);
+ShowArray(arr);
+Console.WriteLine("");
+MinMax(arr);
