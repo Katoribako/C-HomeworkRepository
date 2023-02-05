b307d4a4d67099dfa9430f0a7b109e5d8891b33c
Katoribako <64684310+Katoribako@users.noreply.github.com>
2023-02-05 11:19:57 +0300

    add task27

---
+++ Task27/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные
+//числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+void ShowNums(int number)
+{
+    if (number > 1)
+    {
+        Console.Write(number + ", ");
+        ShowNums(number - 1);
+    }
+    if (number < 1)
+    {
+        Console.Write(number + ", ");
+        ShowNums(number + 1);
+    }
+}
+Console.WriteLine("Please input a number ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n == 1)
+{
+    Console.WriteLine("Please input a correct number ");
+}
+else ShowNums(n);
